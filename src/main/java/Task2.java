e5f6f87151fa90d5087cdaecf23e350d68ff8645
mel-kate <64095870+mel-kate@users.noreply.github.com>
2020-05-05 18:40:31 +0300

    Added Task 2-5 Updated.

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,10 +1,11 @@
 public class Task2 {
     public static void main(String[] args) {
-        String s = "Vladimir";
+        String s = "Kate";
         for (int i = 0; i < 5; i++) {
-            System.out.println(
-                    s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                            + s);
+            for (int j = 0; j < 9; j++) {
+                System.out.print(s + " ");
+            }
+            System.out.println(s + " ");
         }
 
 
--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,12 +1,13 @@
 public class Task3 {
 
     public static void main(String[] args) {
-        String s = "Vladimir";
+        String s = "Kate";
         int i = 0;
         while (i < 5) {
-            System.out.println(
-                    s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                            + s);
+            for (int j = 0; j < 9; j++) {
+                System.out.print(s + " ");
+            }
+            System.out.println(s + " ");
             i++;
         }
 
