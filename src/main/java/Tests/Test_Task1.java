51844ac866b17757328b1a4f564b4a8bb1332db3
mel-kate <64095870+mel-kate@users.noreply.github.com>
2020-05-15 10:50:19 +0300

    Added Task 1 Tests. Situates in Tests folder. DataProviden added.

--- src/main/java/Tests/Test_Task1.java
+++ src/main/java/Tests/Test_Task1.java
@@ -1,23 +1,24 @@
 package main.java.Tests;
 
 import main.java.Task1;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class Test_Task1 {
     Task1 task1;
-    int a1 = 6;
+    /*int a1 = 6;
     int b1 = 1;
-    int c1 = 2;
+    int c1 = 2;*/
 
 
 
-   @Test
-    public void max_int() {
+   @Test(dataProvider = "integers")
+    public void max_int(int a1, int b1, int c1) {
        task1 = new Task1();
         int result = (task1.main(a1,b1,c1));
-        int min = 1;
+        int min = 6;
        assertEquals(
                result,
                min,
@@ -25,6 +26,11 @@ public class Test_Task1 {
        );
 
     }
-
+    @DataProvider(name = "integers")
+    public Object[] getData() {
+        return new Object[][] {
+                {10, 6, 9},
+        };
+    }
 
 }
